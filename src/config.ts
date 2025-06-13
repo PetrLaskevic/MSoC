555e6eec040e9172f42daba8374ba774d96566e0
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-06-13 12:29:44 +0200

    Add types to `config.ts` marking both entries as optional (the inferred type thought both were mandatory)

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,10 @@
 //The configuration for the project you're analyzing
-export let config = {
+interface Config {
+    useGitIgnore?: boolean,
+    otherIgnores?: string[],
+}
+
+export let config: Config = {
     useGitIgnore: true,
     //What the authors wanted to have in git, but what we want to exclude from analysis
     otherIgnores: [

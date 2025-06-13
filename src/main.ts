db88569dbc28f3e6a34b8586f8ba12eecff476c5
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-06-13 19:51:04 +0200

    Support getting (some of) the function calls in the script
    Remove own Node typing information, since acorn has a complete one which works when importing acorn in an ES import way

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,6 @@ async function main(){
         config.otherIgnores = config.otherIgnores.map(
             item => `src/testProjects/yt-anti-translate/${item}`
         );
-        console.log(config.otherIgnores);
         ignores.push(...config.otherIgnores);
     }
     if(config.ecmaScriptVersion){
@@ -26,7 +25,7 @@ async function main(){
     }else{
         console.warn("ES version not specified, defaulting to 2020");
     }
-    readGlobbed("src/testProjects/yt-anti-translate", ignores);
+    // readGlobbed("src/testProjects/yt-anti-translate", ignores);
 }
 
 main();
@@ -42,56 +41,95 @@ async function loadGitIgnore(directory: string) : Promise<string[]> {
 
 }
 
-//wildly simplified typing information (the library doesn't use TS so doesn't provide its own)
-//   Node {
-//     type: 'FunctionDeclaration',
-//     start: 21101,
-//     end: 21590,
-//     id: Node {
-//       type: 'Identifier',
-//       start: 21110,
-//       end: 21146,
-//       name: 'updateObserverOtherVideosOnIntersect'
-//     },
-type Node = { 
-    //CallExpression can be inside od ExpressionStatement 
-    type: "FunctionDeclaration" | "CallExpression" | "ExpressionStatement" | string,
-    start: number,
-    end: number,
-    expression: Node | false,
-
-    name?: string,
-    id?: Node,
-    async?: boolean,
-    //the actual body of the function, which will be type "BlockStatement",
-    //where it will have another body / bodies 
-    body: Node | Node[],
-    params?: any[]
+function getFunctionCallName(callExpressionNode: acorn.CallExpression) : string {
+    let parentNode = callExpressionNode.callee;
+    let wholeName: string[] = [];
+    //leaf of the CallExpression => (sometimes MemberExpression when a.b.c() ) => Identifier chain
+    let leafIdentifierName = ""; 
+    while(parentNode.type != "Identifier"){
+        if(parentNode.type == "MemberExpression"){
+            //for static a.b.c() type of situations
+            //doesn't support a[b].c() yet
+            if(parentNode.property.type == "Identifier"){
+                wholeName.push(parentNode.property.name);
+            }
+            //to continue loop, .object holds the parent, 
+            // so in "a.b.c" it eventually leads us to "a", where parentNode.name will get it
+            parentNode = parentNode.object;
+        }
+    }
+    // the whole name for `callbackify()` when chain is CallExpression.callee => Identifier.name => callbackify
+    // the first part (chrome) when the structure is:
+    //  chrome.storage.sync.get
+    //  CallExpression.callee => MemberExpression .object => MemberExpression .object => MemberExpression .object => Identifier
+    //                             .property: "get"           .property: "sync"           .property: "storage"          .name: "chrome"
+    //  => that's the reason for the while loop above
+    leafIdentifierName = parentNode.name;
+    wholeName.push(leafIdentifierName);
+    wholeName.reverse();
+    return wholeName.join(".");
 }
 function listOfFunctions(jsCode: string) : string[] {
     let functions : string[] = [];
     //while I could loop over the token list an get the top level function declarations, I'm not going to get any functions defined in a function
-    const tokens: Node[] = acorn.parse(jsCode, acornOptions).body;
-    tokens.forEach((node) => {
-        if(node.type == "FunctionDeclaration"){
-            //node.id is another Node with type Identifier
-            let name = node.id.name;
-            functions.push(name);
+    // const tokens: Node[] = acorn.parse(jsCode, acornOptions).body;
+    // console.log(tokens)
+    // tokens.forEach((node) => {
+    //     if(node.type == "FunctionDeclaration"){
+    //         //node.id is another Node with type Identifier
+    //         let name = node.id.name;
+    //         functions.push(name);
+    //     }
+    // });
+    // console.log(tokens);
+
+    walk.simple(acorn.parse(jsCode, acornOptions), {
+        ArrowFunctionExpression(node) {
+            // console.log(`Found an arrow function:`);
+            // console.dir(node, { depth: null });
+        },
+        FunctionExpression(node){
+            // console.log(`Found a function:`)
+            // console.dir(node, { depth: null });
+        },
+        ExpressionStatement(node){
+            // console.log(`Found an expression statement:`);
+            // console.dir(node, { depth: null });
+        },
+        CallExpression(node){
+            console.log(getFunctionCallName(node));
+            //detecting chrome.storage.sync.get
+            
         }
     });
-    // console.log(tokens);
+
+    // console.log("all nodes\n\n\n");
+    // console.dir(acorn.parse(jsCode, acornOptions).body, { depth: null })
+
     return functions;
 }
 
-async function readGlobbed(directory: string, ignores: string[]){
-    console.log(directory, ignores)
-    const jsfiles = await glob([`${directory}/**/*.js`, `${directory}/**/*.ts`], { ignore: ignores });
-    console.log("*********************")
-    for(const filePath of jsfiles){
-        let fileContent = await fs.readFile(filePath, 'utf8');
-        let functions = listOfFunctions(fileContent);
-        if(functions.length == 0){
-            console.log(filePath);
-        }
-    }
+// async function readGlobbed(directory: string, ignores: string[]){
+//     console.log(directory, ignores)
+//     //TODO: test how well acorn supports .ts files (if no, tell the user to compile ts files in js first)
+//     const jsfiles = await glob([`${directory}/**/*.js`, `${directory}/**/*.ts`], { ignore: ignores });
+//     console.log("*********************")
+//     for(const filePath of jsfiles){
+//         let fileContent = await fs.readFile(filePath, 'utf8');
+//         let functions = listOfFunctions(fileContent);
+//         if(functions.length == 0){
+//             console.log(filePath);
+//         }
+//     }
+// }
+
+// src/testProjects/yt-anti-translate/app/src/permission.js
+// src/testProjects/yt-anti-translate/app/src/global.js
+// src/testProjects/yt-anti-translate/app/src/content_start.js
+// src/testProjects/yt-anti-translate/app/src/content_injectglobal.js
+async function test(){
+    let fileContent = await fs.readFile("src/testProjects/yt-anti-translate/app/src/content_start.js", 'utf8');
+    listOfFunctions(fileContent);
 }
+
+test();

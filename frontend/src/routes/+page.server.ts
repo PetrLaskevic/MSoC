17c414a8697b8365a9739664c265d8f65e416251
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-07-18 21:49:14 +0200

    `frontend`: give config to `codeScanner` in parameter (btw, this fixed a horrible bug, where ctrl-r on the page, rerunning codeScanner again,  caused `glob` to give FILES IT HAD IGNORED RIGHTLY ON THE FIRST RUN BEFORE THE RELOAD (`playwright.config.ts`)  (CLEARLY specified in config.ts back when it was hardcoded as a static import from codeScanner folder)
     => this BROKE `acorn`, which first expected things which weren't modules (fixed by adding `sourceType: "module"` to `acornOptions`)
     => then it broke `acorn` again, when it met TypeScript keyword `type` => "unexpected token" => acorn should not be parsing TypeScript
    
     )
    
    Add a rudimentary dark theme

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,7 +1,33 @@
-import { main } from "code-scanner";
+import { main, type Config } from "code-scanner";
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
-	let data = await main();
+
+ 	let config: Config = {
+		analysisTargetDir: "../testProjects/yt-anti-translate",
+		useGitIgnore: true,
+		otherIgnores: [
+			"node_modules",
+			".git",
+			".github",
+			"eslint.config.js",
+			//both playwright links work, the wildcards are not needed, the ignores are evaluated with analysisTargetDir as base directory
+			"**/playwright.config.ts",
+			"playwright.config.js",
+			"tests/**", 
+			// "!(permission.js)", not supported by glob lib
+			//so: ls testProjects/yt-anti-translate/**/**/.js an then removing the permission.js file
+			// "app/src/background_audio.js",
+			// "app/src/background_description.js",
+			// "app/src/background.js",
+			// "app/src/content_channelbranding.js",
+			// "app/src/content_injectglobal.js",
+			// "app/src/content_start.js",
+			// "app/src/global.js",
+			// "app/src/options.js",
+			// "app/src/permission.js",
+		]
+	}
+	let data = await main(config);
 	return { data };
 }

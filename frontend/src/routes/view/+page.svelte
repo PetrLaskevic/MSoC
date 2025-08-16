0d3843a1aec4b19f2660c4715a2ac38785c5209c
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-08-16 14:48:50 +0200

    `frontend`: Add tab title to fullscreen diagram view page

--- frontend/src/routes/view/+page.svelte
+++ frontend/src/routes/view/+page.svelte
@@ -2,11 +2,13 @@
   import View from '$/components/View.svelte';
   import { initHandler } from '$/util/util';
   import { onMount } from 'svelte';
+  import { stateStore } from '$/util/state';
   onMount(initHandler);
 </script>
 
 <svelte:head>
   <meta name="robots" content="noindex" />
+  <title>{"BirdsEye - " + $stateStore.filePath!.split("/").pop()}</title>
 </svelte:head>
 
 <View shouldShowGrid={false} />
